3ce04c20abb290f1482652c38832502e86ac815b
johntuan <john_ha80@hotmail.com>
2020-11-06 01:01:46 +0000

    changes for deployment

--- config/database.rb
+++ config/database.rb
@@ -1,25 +1,32 @@
-# A sample Gemfile
-source "https://rubygems.org"
+configure do
+  # Log queries to STDOUT in development
+  if Sinatra::Application.development?
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
 
-gem 'rake'
-gem 'activesupport'
-gem 'activerecord', '~> 4.2.0'
+  if Sinatra::Application.development?
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    db_url = 'postgres://iqilrxrpjkxhgd:b7266d8c906509fe5c7eac295813582fa4ae76c55eb357d123352491c4f3ce1f@ec2-54-224-124-241.compute-1.amazonaws.com:5432/dbacdtqtg4klgv'
+    db = URI.parse(ENV['DATABASE_URL'] || db_url)
+    set :database, {
+      adapter: "postgresql",
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
 
-gem 'sinatra'
-gem 'sinatra-contrib'
-gem 'sinatra-activerecord'
+  # Load all models from app/models, using autoload instead of require
+  # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
+  Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+    filename = File.basename(model_file).gsub('.rb', '')
+    autoload ActiveSupport::Inflector.camelize(filename), model_file
+  end
 
-gem 'puma'
-gem 'tux'
-
-gem 'pry'
-
-group :development, :test do
-  gem 'shotgun'
-  gem 'sqlite3', '~> 1.3.6'
-end
-
-group :production do 
-  gem 'rails_12factor'
-  gem 'pg', '~> 0.20'
 end
