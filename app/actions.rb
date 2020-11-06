ae09e7547d804dcd8a00f8d6617bace0a885ab2a
johntuan <john_ha80@hotmail.com>
2020-11-06 00:46:47 +0000

    changes for deployment

---
+++ app/actions.rb
@@ -0,0 +1,125 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+#on this section We can do one more thing to make this code a little more useful, but first we need to cover one simple concept.
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes >=60
+        "#{time_ago_in_minutes / 60} hours ago"
+    else
+        "#{time_ago_in_minutes} minutes ago"
+    end
+end
+
+delete '/likes/:id' do
+    like= Like.find(params[:id])
+    like.destroy
+    redirect(back)
+end
+
+get '/' do
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
+end
+
+get '/signup' do 
+    @user = User.new
+    erb(:signup)
+
+end
+
+get '/login' do
+    erb(:login)
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id]) #find the finstagram post with the ID from the URL
+    erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+    # point values from params to variables
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+
+    # instantiate a comment with those values & assign the comment to the `current_user`
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+    # save the comment
+    comment.save
+
+    # `redirect` back to wherever we came from
+    redirect(back)
+end
+
+post '/likes' do
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    like.save
+
+    redirect(back)
+end
+
+post '/finstagram_posts' do 
+    photo_url = params[:photo_url]
+
+    #instantiate new FinstagramPost
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+    #if @post validates, save
+    if @finstagram_post.save
+        redirect(to('/'))
+    else
+        erb(:"finstagram_posts/new")
+    end
+    
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    #1 Find user by username
+    user = User.find_by(username: username)
+
+    #2. If that user exists
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
+post '/signup' do
+    
+    #grab user input values from params
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+
+    #instantitate and save a User
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    #if user validations pass and user is saved
+    if @user.save
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
+
+end
